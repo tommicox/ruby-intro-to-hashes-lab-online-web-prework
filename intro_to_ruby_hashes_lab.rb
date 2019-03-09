0328d63d05f38e3840e2d3cd96aa8a98a09e3afd
Tommi Cox <tommicox@gmail.com>
2019-03-09 14:57:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -105,10 +105,7 @@ def monopoly_with_fourth_tier
 	     railroads: {
 	       pieces: 4,
 	       names: {
-	         :reading_railroad => {
-	           "mortgage_value" => "100"
-	           
-	         },
+	         :reading_railroad => {"mortgage_value" => "100"},
 	         :pennsylvania_railroad => {"mortgage_value" => "200"},
 	         :b_and_o_railroad => {"mortgage_value" => "400"},
 	         :shortline => {"mortgage_value" => "800"}
@@ -124,7 +121,7 @@ def monopoly_with_fourth_tier
 		
 end
 	
-end
+
 
 
 
